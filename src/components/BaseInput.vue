c35d2c4516ab6ce4f1945e4a5a895e4ecb099d43
emmartin <emmartin@paradigmadigital.com>
2021-08-31 16:11:28 +0200

    create service, autoimport base components, and add form

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <input
+      :placeholder="label"
+      class="field"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+      v-bind="$attrs"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+  },
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import EventDetail from '../views/EventDetail.vue'
 import EventList from '../views/EventList.vue'
+import SimpleForm from '../views/SimpleForm.vue'
 
 const routes = [
   {
@@ -13,8 +15,20 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
+    // perfomance optimization!!
     component: () =>
       import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/event/:id',
+    name: 'EventDetail',
+    props: true,
+    component: EventDetail,
+  },
+  {
+    path: '/form',
+    name: 'SimpleForm',
+    component: SimpleForm,
   },
 ]
 
